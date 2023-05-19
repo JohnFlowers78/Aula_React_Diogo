39e5edd726e79ab189e4bbe1008f2d2d27ced78a
JohnFlowers78 <jpsflores2000@gmail.com>
2023-05-19 10:19:25 -0300

    Aula_React_19_05

--- ambientacao-react/src/components/Soma.tsx
+++ ambientacao-react/src/components/Soma.tsx
@@ -1,5 +1,14 @@
+//Todo componente deve ter a primeira letra maiúscula
+//Todo componente deve ter uma função que retorna um HTML
+//Todo componente deve ser exportado
+//Todo componente deve rettornar apenas um elemento pai HTML
 function Soma(){
-    return (<h2>Função de soma</h2>);
+    return (
+        <div>
+            <h1> Componente da soma </h1>
+            <label></label>
+        </div>
+    );
 }
 
 // Criar duas caixas de texto
